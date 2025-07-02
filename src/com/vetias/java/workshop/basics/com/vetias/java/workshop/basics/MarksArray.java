389f39ecf3aee5e60f39a54622cb3191f722988c
Rajadurai <k.rajadurai@gmail.com>
2025-07-02 14:43:12 +0530

    Arrays example

---
+++ src/com/vetias/java/workshop/basics/com/vetias/java/workshop/basics/MarksArray.java
@@ -0,0 +1,11 @@
+package com.vetias.java.workshop.basics;
+
+public class MarksArray {
+
+    public static void main(String[] args) {
+        int []marks = {90,98,96,89,91};
+        for(int mark:marks) {
+            System.out.println(mark);
+        }
+    }
+}
